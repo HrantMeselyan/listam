15b1ed930fbab697507ded2f36c9c24916fec098
dell <hrant3307@gmail.com>
2023-05-08 13:04:09 +0400

    list.am

--- src/main/java/com/example/listam/controller/CommentController.java
+++ src/main/java/com/example/listam/controller/CommentController.java
@@ -6,12 +6,10 @@ import com.example.listam.repository.CommentRepository;
 import com.example.listam.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
